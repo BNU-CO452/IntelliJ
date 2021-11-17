f3f871d4faf1504ff5483e41bdfe22e7420e475c
Derek Peacock <derek.peacock@yahoo.com>
2021-11-17 16:17:01 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Example of using IntelliJ instead of BlueJ
+ */
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    StockApp app = new StockApp();
+        app.run();
+    }
+}
